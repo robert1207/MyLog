a46a4bae319950c633d6eeafecface5f452c00e9
Robert <robert.cysy@gmail.com>
2019-06-28 16:23:39 +0800

    update header of code

--- src/file_logger.h
+++ src/file_logger.h
@@ -1,11 +1,11 @@
 /* MyLog is a log-lib for Qt applications.
  * Copyright (C) <2019>  <Robert Zhang> e-mail:robert.cysy@gmail.com
-
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
